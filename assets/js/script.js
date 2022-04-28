37c6881da468f1d2f6749afe097d707d160954d7
Henry Leon <henryatusabes@gmail.com>
2022-04-28 22:01:04 +0000

    Add link to the html file for favicon

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     }
 
-    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+    document.getElementById("answer-box").addEventListener("keydown", function (event) {
         if (event.key === "Enter") {
             checkAnswer();
         }
